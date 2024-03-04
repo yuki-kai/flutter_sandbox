34bd7c31678daef6bf11e60128dd0a36845db7fd
Yuki Ishihara <kai@exciter.jp>
2024-03-04 13:58:17 +0900

    supabaseとの連携

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_sandbox/router.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+
+  await Supabase.initialize(
+    anonKey: dotenv.env['SUPABASE_ANON'] ?? '',
+    url: dotenv.env['SUPABASE_URL'] ?? '',
+  );
+
   runApp(const MyApp());
 }
 
